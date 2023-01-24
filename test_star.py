bec618aa972f3f37b96c95a08e083983ca7f2adc
Burak Cuhadar <burak.cuhadar@tum.de>
2023-01-24 15:13:40 +0100

    ate rpe evaluation

--- test_star.py
+++ test_star.py
@@ -8,6 +8,75 @@ from datasets import dataset_dict
 from utils.io import *
 from utils.logging import LoggerWandb
 from torch.utils.data import DataLoader
+from utils.metrics import evaluate_trajectory
+from lietorch import SO3
+
+
+def evaluate_rpe(star_poses, gt_poses):
+    '''
+        star_poses: [num_frames, 6], torch.Tensor
+        gt_poses: [num_frames, 4, 4]
+    '''
+    assert star_poses.shape[0] == gt_poses.shape[0]
+    num_frames = gt_poses.shape[0]
+
+    traj_gt = {}
+    traj_est = {}
+    for i in range(num_frames):
+        traj_gt[i] = gt_poses[i]
+
+        mat_est = np.eye(4, dtype=np.float32)
+        mat_est[:3,:3] = SO3.exp(star_poses[i,3:]).matrix().cpu().detach().numpy()[:3,:3] 
+        mat_est[:3,3] = star_poses[i,:3].cpu().detach().numpy() 
+        traj_est[i] = mat_est
+    
+    result = evaluate_trajectory(traj_gt,
+                                 traj_est,
+                                 param_max_pairs=10000,
+                                 param_fixed_delta=True, #TODO?
+                                 param_delta=1.00)
+
+    #stamps = numpy.array(result)[:,0]
+    trans_error = np.array(result)[:,4]
+    rot_error = np.array(result)[:,5]
+
+    print("RPE EVALUATION:")
+    print("compared_pose_pairs %d pairs"%(len(trans_error)))
+
+    print("translational_error.rmse %f m"%np.sqrt(np.dot(trans_error,trans_error) / len(trans_error)))
+    print("translational_error.mean %f m"%np.mean(trans_error))
+    print("translational_error.median %f m"%np.median(trans_error))
+    print("translational_error.std %f m"%np.std(trans_error))
+    print("translational_error.min %f m"%np.min(trans_error))
+    print("translational_error.max %f m"%np.max(trans_error))
+
+    print("rotational_error.rmse %f deg"%(np.sqrt(np.dot(rot_error,rot_error) / len(rot_error)) * 180.0 / np.pi))
+    print("rotational_error.mean %f deg"%(np.mean(rot_error) * 180.0 / np.pi))
+    print("rotational_error.median %f deg"%(np.median(rot_error) * 180.0 / np.pi))
+    print("rotational_error.std %f deg"%(np.std(rot_error) * 180.0 / np.pi))
+    print("rotational_error.min %f deg"%(np.min(rot_error) * 180.0 / np.pi))
+    print("rotational_error.max %f deg"%(np.max(rot_error) * 180.0 / np.pi))
+
+    
+def evaluate_ate(star_poses, gt_poses):
+    '''
+        star_poses: [num_frames, 6], torch.Tensor
+        gt_poses: [num_frames, 6]
+    '''
+    assert star_poses.shape[0] == gt_poses.shape[0]
+
+    gt_trans = gt_poses[:,:3].cpu().detach().numpy()
+    star_trans = star_poses[:,:3].cpu().detach().numpy() 
+    diff = (star_trans - gt_trans).T # [3, num_frames]
+    trans_error = np.sqrt(np.sum(diff * diff, 0))
+    
+    print("compared_pose_pairs %d pairs"%(len(trans_error)))
+    print("absolute_translational_error.rmse %f m"%np.sqrt(np.dot(trans_error,trans_error) / len(trans_error)))
+    print("absolute_translational_error.mean %f m"%np.mean(trans_error))
+    print("absolute_translational_error.median %f m"%np.median(trans_error))
+    print("absolute_translational_error.std %f m"%np.std(trans_error))
+    print("absolute_translational_error.min %f m"%np.min(trans_error))
+    print("absolute_translational_error.max %f m"%np.max(trans_error))
 
 
 
@@ -44,6 +113,10 @@ def test():
         video_frames_savedir = os.path.join(args.basedir, args.expname+'_test', 'video_frames')
         os.makedirs(video_frames_savedir, exist_ok=True)
 
+    evaluate_rpe(star_model.get_poses()[1:], render_video_dataset.gt_relative_poses_matrices)
+    evaluate_ate(star_model.get_poses()[1:], render_video_dataset.gt_relative_poses[1:])
+
+    
     # Create video
     print('Starting video rendering')
     rgbs, disps, rgb_statics0, rgb_dynamics0, rgb_statics, rgb_dynamics = render_path_star(render_video_dataloader, 
@@ -62,6 +135,7 @@ def test():
     logger_wandb.log_video(rgb_path, disp_path, rgb_static_path, rgb_dynamic_path)
     
     #TODO log rgb_static0/dynamic0 video as well?
+    
     
 
 if __name__=='__main__':
